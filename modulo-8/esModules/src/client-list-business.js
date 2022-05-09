f29a0bbbf5b5370bd402ee2864502a00108a2421
GCobo <guillermo92cobo@gmail.com>
2022-05-09 17:48:17 +0200

    create commonJS and esModules folders

---
+++ modulo-8/esModules/src/client-list-business.js
@@ -0,0 +1,13 @@
+import { getClientElement } from './client-business.js'
+import { getClients } from './data-business.js'
+
+export function printClientsAccounts() {
+  const clients = getClients()
+  const ul = document.createElement('ul')
+  for (let client of clients) {
+    const element = getClientElement(client)
+    ul.appendChild(element)
+  }
+
+  document.getElementById('root').appendChild(ul)
+}
