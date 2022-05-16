34562ef84b3604f6e7dce72f1f96c9abc5eeaf58
GCobo <guillermo92cobo@gmail.com>
2022-05-16 17:38:08 +0200

    data

--- modulo-9/rick&morty/src/data.js
+++ modulo-9/rick&morty/src/data.js
@@ -1,39 +1,26 @@
 import axios from 'axios'
 import { createCard, createPagination } from './utils.js'
 
-let url = 'https://rickandmortyapi.com/api/character'
+const url = 'https://rickandmortyapi.com/api/character'
 const getList = document.querySelector('#character')
 const getPagination = document.querySelector('#pagination')
+let pages
+let data = []
 
-const callUrl = (url) => {
-  return axios.get(url)
+const getCharacters = async (url) => {
+  const promise = await axios.get(url)
+  data = promise.data.results
+  data.map((item) => getList.append(createCard(item)))
 }
 
-export const getData = () => {
-  let data = []
-  let pagination = 1
-
-  callUrl(url).then((response) => {
-    data = response.data.results
-    pagination = response.data.info.pages
-
-    // create card
-    data.map((item) => {
-      getList.append(createCard(item))
+export const createPage = async () => {
+  const promise = await axios.get(url)
+  pages = promise.data.info.pages
+  for (let i = 0; i < pages; i++) {
+    getPagination.append(createPagination(i + 1))
+    const getButton = document.getElementById(`page-${i + 1}`)
+    getButton.addEventListener('click', () => {
+      getCharacters(`${url}?page=${i + 1}`)
     })
-    // create pagination
-    for (let i = 0; i < pagination; i++) {
-      getPagination.append(createPagination(i + 1))
-      document.getElementById(`page-${i + 1}`).addEventListener('click', () => {
-        data = []
-        callUrl(`${url}?page=${i + 1}`).then((response) => {
-          data = response.data.results
-
-          data.map((item) => {
-            getList.append(createCard(item))
-          })
-        })
-      })
-    }
-  })
+  }
 }
--- modulo-9/rick&morty/src/index.js
+++ modulo-9/rick&morty/src/index.js
@@ -1,6 +1,10 @@
 import '../index.css'
-import { getData } from './data'
+// import { getData } from './data'
+// window.onload = function () {
+//   getData()
+// }
+import { createPage } from './data'
 
 window.onload = function () {
-  getData()
+  createPage()
 }
