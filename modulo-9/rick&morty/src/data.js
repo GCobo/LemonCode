cbce79be46dad839aaf874ce230eced7959217c3
GCobo <guillermo92cobo@gmail.com>
2022-06-25 19:42:07 +0200

    rick y morty async

--- modulo-9/rick&morty/src/data.js
+++ modulo-9/rick&morty/src/data.js
@@ -1,33 +1,40 @@
-import axios from 'axios'
-import { createCard, createPagination } from './utils.js'
+import { createCard, createPagination } from "./utils.js";
 
-const url = 'https://rickandmortyapi.com/api/character'
-const getList = document.querySelector('#character')
-const getPagination = document.querySelector('#pagination')
-let pages
-let data = []
+const apiUrl = "https://rickandmortyapi.com/api/character";
+const characters = document.getElementById("character");
+const pagination = document.getElementById("pagination");
+let page = "1";
 
-const getCharacters = async (url) => {
-  const response = await axios.get(url)
-  data = response.data.results
-  data.map((item) => getList.append(createCard(item)))
-}
+const getCharacters = async (innerUrl = page) => {
+  try {
+    let response = await fetch(`${apiUrl}?page=${innerUrl}`);
+    const data = await response.json();
+    data.results.map((item) => {
+      characters.appendChild(createCard(item));
+    });
+  } catch {
+    characters.innerHTML = "There was an error";
+  }
+};
 
 export const createPage = async () => {
-  const response = await axios.get(url)
-  pages = response.data.info.pages
-  getCharacters(`${url}`)
+  getCharacters();
 
-  for (let i = 0; i < pages; i++) {
-    getPagination.append(createPagination(i + 1))
-    const getButton = document.getElementById(`page-${i + 1}`)
-    getButton.addEventListener('click', () => {
-      getList.innerHTML = ''
+  try {
+    let response = await fetch(`${apiUrl}`);
+    const data = await response.json();
 
-      getCharacters(`${url}?page=${i + 1}`)
-    })
+    for (let i = 1; i <= data.info.pages; i++) {
+      pagination.appendChild(createPagination(i));
+      let idItem = await document.getElementById(`page-${i}`);
+      idItem.addEventListener("click", () => {
+        characters.innerHTML = "";
+        getCharacters(`${i}`);
+      });
+    }
+  } catch {
+    pagination.innerHTML = "There was an error";
   }
-}
+};
 
 // añadir detalle de cada personaje llamando a otro endpoint
-
